7cfade5e3d5c5187b8c07cc2587a1411a0f60ad6
bbaa <bbaa_bbaa@qq.com>
2019-07-03 15:37:35 +0800

    完整框架

--- qqPic.php
+++
@@ -1,6 +0,0 @@
-<?php
-$QQ=$_GET["qq"];
-header("Cache-Control: max-age=2592000");
-header("Content-Type: image/jpeg");
-header("Server: ImgHttp3.0.0");
-echo file_get_contents("http://q2.qlogo.cn/headimg_dl?bs={$QQ}&dst_uin={$QQ}&spec=100");
